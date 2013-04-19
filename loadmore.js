687b9a50d9abd40d9a60344c2737ea3a943f8849
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2013-04-18 22:46:57 -0300

    added better event methods, using jQuery trigger

--- loadmore.js
+++ loadmore.js
@@ -1,30 +1,28 @@
 /*
 * jQuery loadmore
-* https://github.com/carlosrberto/jquery-loadmore
+* http://carlosrberto.github.com/jquery-loadmore/
 *
-* Copyright (c) 2012 Carlos Roberto Gomes Junior
+* Copyright (c) 2013 Carlos Roberto Gomes Junior
 * http://carlosroberto.name/
 * 
 * Licensed under a Creative Commons Attribution 3.0 License
 * http://creativecommons.org/licenses/by-sa/3.0/
 *
-* Version: 0.1
+* Version: 1.0
 */
 
 (function($){
+    var rscript = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+
     var defaults = {
-        url: '/path/',
+        url: '',
         pageVar: 'page',
+        extraVars: {},
         pageNumber: 1,
-        maxPages: 1000,
+        maxPages: 10,
         autoAppend: true,
-        onLoad: function() {},
-        onError: function() {},
-        onBeforeLoad: function() {},
-        onBeforeAppend: function(dataFragment) {},
-        onAfterAppend: function() {},
-        onAfterPageIncrement: function() {},
-        onEndPages: function() {}
+        selector: null,
+        buttonSelector: null
     };
     
     function LoadMore(container, options){
@@ -41,56 +39,91 @@
         that.pageVar = that.settings.pageVar;
         that.container = $(container);
         that.isLoading = false;
+
+        that.initButtonEvents();
     }
 
     LoadMore.prototype = {
+
+        initButtonEvents: function() {
+            if ( typeof this.settings.buttonSelector === 'string' ) {
+                $('body').on('click', this.settings.buttonSelector, $.proxy(function( event ) {
+                    event.preventDefault();
+                    this.load();
+                }, this));
+            }
+        },
+
+        setOptions: function( options ) {
+            this.settings = $.extend({}, this.settings, options);
+        },
+
         load: function() {
-            var that = this;
-            if ( that.isLoading || that.pageNumber > that.settings.maxPages ) {
+            var that = this, data = {};
+            
+            if ( that.isLoading || that.settings.pageNumber > that.settings.maxPages ) {
                 return;
             }
+
             that.isLoading = true;
-            var data = {};
-            data[that.pageVar] = that.pageNumber;
+                    
+            $.extend(data, that.settings.extraVars);
+            data[that.settings.pageVar] = that.settings.pageNumber;
             $.ajax({
                 type: "get",
-                url: that.url,
+                url: that.settings.url,
                 data: data,
+
                 beforeSend:function(){
-                    that.settings.onBeforeLoad.call(that.container);
+                    that.container.trigger('beforeload.loadmore');
                 },
-                success: function(data){
-                    if ( that.pageNumber+1 > that.settings.maxPages ) {
-                        that.settings.onEndPages.call(this);
-                    }                     
+                
+                success: function(data){                     
                     that._processData(data);
-                    that.settings.onLoad.call(that.container);               
                 },
+
                 error: function(jqXHR) {
                     that.isLoading = false;
-                    that.settings.onError.call(that.container, jqXHR);
+                    that.container.trigger('error.loadmore', [jqXHR]);
                 }
             });
         },
 
+        _parseHTML: function( string ) {
+            return string.replace(rscript, "");
+        },        
+
         _processData: function(data) {
-            var that = this;
-            var fragment = $('<div>');
-            fragment.append($(data));
-            that.settings.onBeforeAppend.call(this, fragment);
-            if ( that.settings.autoAppend ) {
-                that.container.append(fragment.html());
-                that.settings.onAfterAppend.call(this);
+            var that = this,
+                fragment = $('<div>'),
+                html;
+
+            fragment.append( this._parseHTML( data ) );
+            
+            if ( typeof that.settings.selector === 'string' ) {
+                html = fragment.find( that.settings.selector ).html();
+            } else {
+                html = fragment.html();
             }
-            that.pageNumber++;
+
+            if ( that.settings.autoAppend ) {
+                that.container.append( html );
+            }
+
+            that.settings.pageNumber++;
             that.isLoading = false;
-            that.settings.onAfterPageIncrement.call(this);
+            that.container.trigger('load.loadmore', [ html, that.settings.pageNumber ]);
+            
+            if ( that.settings.pageNumber > that.settings.maxPages ) {
+                that.container.trigger('endpages.loadmore');
+            }            
         }
-    };
+    }
 
     $.fn.loadmore = function( method ) {
+        var args = arguments;
         return this.each(function() {
-            // TODO: find a way to do this better
+
             if ( !$(this).data('loadMore') ) {
                 $(this).data('loadMore', new LoadMore(this, method));
                 return;
@@ -98,13 +131,11 @@
             
             var api = $(this).data('loadMore');
             
-            if ( api[ method ] ) {
-                api[ method ].apply( api, Array.prototype.slice.call( arguments, 1 ) );
+            if ( typeof method === 'string' && method.charAt(0) !== '_' && api[ method ] ) {
+                api[ method ].apply( api, Array.prototype.slice.call( args, 1 ) );
             } else {
                 $.error( 'Method ' +  method + ' does not exist on jQuery.loadmore' );
             }
         });
     };
-
-
 })(jQuery);
